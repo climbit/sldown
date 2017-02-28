cc125dabd39ceb6e90c969b948f9defb332e4def
Daniel <climbit@gmx.de>
2017-02-28 15:44:18 +0100

    Generate download urls. Modify argparse entry for the file type.

--- sldown.py
+++ sldown.py
@@ -6,20 +6,27 @@ import csv
 
 parser = argparse.ArgumentParser(description='Download the books, which are specified in the csv file. The file have to be a CSV file downloaded at the link.springer.com page. This works only if access is granted.')
 parser.add_argument('csvfil', type=str, metavar='FILE', help='CSV input file')
-parser.add_argument('-t', '--ftype', dest='filetype', type=str, metavar='FILETYPE', help='Specify the file type epub or download. For multiple use a comma separated list.')
+parser.add_argument('ftype',  type=str, metavar='FILETYPE', help='Specify the file type (pdf or epub) to download. For multiple use a comma separated list.')
 
 def sld(argCsvFile, argFiletype):
     if not os.path.exists(argCsvFile):
         logging.error('File does not exist: '+argCsvFile)
         return
     
+    filTypeList = argFiletype.replace(' ','').lower().split(',')
+    
     with open(argCsvFile, 'rb') as csvFile:
-        #csvContent = csv.reader(csvFile, delimiter=',', quotechar='"')
         csvContent = csv.DictReader(csvFile, delimiter=',', quotechar='"')
         for row in csvContent:
-            print row['Item Title'] + ", " + row['URL']
+            for filtype in filTypeList:
+                if filtype == 'pdf':
+                    url = row['URL'].replace('book', 'content/pdf')+'.pdf'
+                elif filtype == 'epub':
+                    url = row['URL'].replace('book', 'download/epub')+'.epub'
+                else:
+                    logging.error('Filytype not supported: '+filtype)
     
 ####################################################################################################
 if __name__ == '__main__':
     args = parser.parse_args()
-    sld(args.csvfil, args.filetype)
+    sld(args.csvfil, args.ftype)
